3b0149e3c42ef340e835aceedc493e881b347eb3
Alexandr <AlexandrKsenz@gmail.com>
2025-09-02 16:43:44 +0200

    added validation on todo page

--- src/components/UI/Input/Input.js
+++ src/components/UI/Input/Input.js
@@ -2,13 +2,19 @@ import React from 'react';
 import classNames from 'classnames';
 import styles from './Input.module.scss';
 
-function Input({ value, onChange, text, className }) {
+function Input({ name, value, onChange, text, className, placeholder, val }) {
     return (
         <input
+            name={name}
             value={value}
             onChange={onChange}
             type={text}
-            className={classNames(styles.input, className)} />
+            placeholder={placeholder}
+            className={classNames(
+                styles.input,
+                className,
+                { [styles.input__error]: val }
+            )} />
     )
 }
 
--- src/components/ordinary/modal/Modal.js
+++ src/components/ordinary/modal/Modal.js
@@ -10,8 +10,16 @@ import styles from './Modal.module.scss';
 
 function Modal({ supabase, getTodo, modal, setModal }) {
     const { v4: uuidv4 } = require('uuid');
-    const [title, setTitle] = useState('');
-    const [descr, setDescr] = useState('');
+
+    const [task, setTask] = useState({
+        title: '',
+        descr: '',
+    })
+    const [error, setError] = useState({
+        title: false,
+        descr: false,
+    })
+
     const [date, setDate] = useState(() => {
         const date = new Date();
         const month = (date.getMonth() + 1).toString();
@@ -21,28 +29,94 @@ function Modal({ supabase, getTodo, modal, setModal }) {
 
 
     async function addTask() {
-        const { data, error } = await supabase
-            .from('todo')
-            .insert([
-                { id: uuidv4(), created_at: date, title: title, descr: descr, isDone: false }
-            ])
+        if (validate()) {
+            const { data, error } = await supabase
+                .from('todo')
+                .insert([
+                    {
+                        id: uuidv4(),
+                        created_at: date,
+                        title: task.title,
+                        descr: task.descr,
+                        isDone: false
+                    }
+                ])
 
-        if (error) {
-            console.error('Ошибка:', error.message)
-        } else {
-            getTodo();
-            setTitle('');
-            setDescr('');
-            setModal(false);
+            if (error) {
+                console.error('Ошибка:', error.message)
+            } else {
+                getTodo();
+                setTask({
+                    title: '',
+                    descr: '',
+                })
+                setModal(false);
+            }
         }
+    }
+
+    const validate = () => {
+        let hasError = false;
+
+        Object.entries(task).forEach(([name, val]) => {
+            if (!val || val.length === 0) {
+                setError(prev => ({ ...prev, [name]: true }));
+                hasError = true;
+            } else {
+                setError(prev => ({ ...prev, [name]: false }));
+            }
+        });
+
+        return !hasError; // true = все ок
+    };
+
+    // const validation = (name, val) => {
+    //     if (!val || val.length === 0) {
+    //         setError(prevState => ({
+    //             ...prevState,
+    //             [name]: true,
+    //         }))
+    //     } else {
+    //         setError(prevState => ({
+    //             ...prevState,
+    //             [name]: false,
+    //         }))
+    //     }
+    // }
+
+    const onChange = (event) => {
+        const name = event.target.name;
+        const val = event.target.value;
+
+        setTask(prevState => ({
+            ...prevState,
+            [name]: val,
+        }));
+
+        validate(name, val)
     }
 
     return <div className={modal ? styles.modal + ' ' + styles.modal__active : styles.modal}>
         <div className={styles.block}>
             <h3 className={styles.title}>Add task</h3>
             <div className={styles.group}>
-                <Input value={title} onChange={(e) => setTitle(e.target.value)} type="text" className="marginCenter marginBottom" />
-                <Input value={descr} onChange={(e) => setDescr(e.target.value)} type="text" className="marginCenter marginBottom" />
+                <Input
+                    name="title"
+                    value={task.title}
+                    placeholder="Task title"
+                    onChange={onChange}
+                    type="text"
+                    val={error.title}
+                    className="marginCenter marginBottom"
+                />
+                <Input
+                    name="descr"
+                    value={task.descr}
+                    placeholder="Your task"
+                    onChange={onChange}
+                    type="text"
+                    val={error.descr}
+                    className="marginCenter marginBottom" />
             </div>
             <button type="button" className={styles.close} onClick={() => setModal(false)}>X</button>
             <Button type="button" text="Confirm" onClick={() => addTask()} className="marginCenter" />
